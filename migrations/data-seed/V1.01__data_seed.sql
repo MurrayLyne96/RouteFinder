be92348f4264598eb9869fd4eb2c8d6b484d9aba
MurrayLyne96 <murray.lyne@unosquare.com>
2022-11-24 11:33:57 +0000

    Add dataseed
    Added a dataseed file and split the migration files into 2 folders. Base and data-seed.

---
+++ migrations/data-seed/V1.01__data_seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO users(email_address, first_name, last_name, password, role, date_of_birth)
+VALUES('testuser@test.com', 'test', 'user', 'password01', 'ADM', '1996-03-15 00:00:00');
+
+INSERT INTO types(name)
+VALUES ('Running');
+
+INSERT INTO types(name)
+VALUES ('Cycling');
+
+INSERT INTO tags(name)
+VALUES ('Hilly');
+
+INSERT INTO tags(name)
+VALUES ('Flat');
+
+INSERT INTO tags(name)
+VALUES ('Long');
+
+INSERT INTO tags(name)
+VALUES ('Woodland');
+
+INSERT INTO tags(name)
+VALUES ('Urban');
+
+INSERT INTO tags(name)
+VALUES ('Mountains');
+
+INSERT INTO tags(name)
+VALUES ('Coastal');
