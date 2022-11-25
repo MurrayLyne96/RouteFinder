b6029d18da58cb2ffb9b44230ae4ddcadc117cf4
MurrayLyne96 <murray.lyne@unosquare.com>
2022-11-25 09:59:00 +0000

    Add API solution
    .net 6.0 API with no authenticated configured to begin with

--- RouteFinderAPI/Controllers/WeatherForecastController.cs
+++ RouteFinderAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace RouteFinder.Controllers;
+namespace RouteFinderAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- RouteFinderAPI/WeatherForecast.cs
+++ RouteFinderAPI/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace RouteFinder;
+namespace RouteFinderAPI;
 
 public class WeatherForecast
 {
