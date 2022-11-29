3d79bf6d017490bd2b7aa7e7862da9b2a0385955
MurrayLyne96 <murray.lyne@unosquare.com>
2022-11-29 15:29:18 +0000

    Add validation for base properties

--- RouteFinderAPI/RouteFinderAPI.Models/ViewModels/Base/BaseViewModel.cs
+++ RouteFinderAPI/RouteFinderAPI.Models/ViewModels/Base/BaseViewModel.cs
@@ -5,3 +5,12 @@ public class BaseViewModel
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime LastModified { get; set; } = DateTime.Now;
 }
+
+public class BaseValidator : AbstractValidator<BaseViewModel>
+{
+    public BaseValidator()
+    {
+        RuleFor(x => x.Created).NotEmpty().NotNull();
+        RuleFor(x => x.LastModified).NotEmpty().NotNull();
+    }
+}
