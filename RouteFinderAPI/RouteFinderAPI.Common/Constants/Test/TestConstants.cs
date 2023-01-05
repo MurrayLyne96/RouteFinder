ab65a5f0b346a5b4773b64a97c24d9ad4c744ce6
MurrayLyne96 <murray.lyne@unosquare.com>
2023-01-05 18:40:06 +0000

    Add constants for tests

---
+++ RouteFinderAPI/RouteFinderAPI.Common/Constants/Test/TestConstants.cs
@@ -0,0 +1,7 @@
+namespace RouteFinderAPI.Common.Constants.Test;
+
+public static class TestConstants {
+    public const string DevAuthenticationScheme = "SuperUserTest";
+    public const string DevName = "Test";
+    public const string DevEmail = "Test@TestEmail.com";
+}
