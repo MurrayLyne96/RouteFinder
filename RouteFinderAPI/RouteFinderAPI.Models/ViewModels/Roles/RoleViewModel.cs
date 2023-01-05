aa54100a739c1fb8f6f5f1a05cf8fd64a6a2c596
MurrayLyne96 <murray.lyne@unosquare.com>
2023-01-05 18:39:55 +0000

    Fix data type

--- RouteFinderAPI/RouteFinderAPI.Models/ViewModels/Roles/RoleViewModel.cs
+++ RouteFinderAPI/RouteFinderAPI.Models/ViewModels/Roles/RoleViewModel.cs
@@ -2,7 +2,7 @@ namespace RouteFinderAPI.Models.ViewModels.Roles;
 
 public class RoleViewModel
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
 
     public string RoleName { get; set; }
 
