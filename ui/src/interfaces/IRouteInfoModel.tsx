bbf594788a581d57169ead89bce34bff193c0cbb
MurrayLyne96 <murray.lyne@unosquare.com>
2023-02-03 16:50:50 +0000

    Add route info model
    The route info model is for getting information exclusive to the google maps API, so it is kept separate from the main model.

---
+++ ui/src/interfaces/IRouteInfoModel.tsx
@@ -0,0 +1,7 @@
+export interface IRouteInfoModel {
+    totalDistance: string,
+    totalElevation: string,
+    lowestElevation: string,
+    highestElevation: string,
+    timeToComplete: string,
+}
