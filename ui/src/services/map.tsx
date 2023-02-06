919d018cf7a253f5d211b05c30ac7929ff2d2a58
MurrayLyne96 <murray.lyne@unosquare.com>
2023-02-06 10:23:32 +0000

    Allow travel mode to be different based on Route Type

--- ui/src/services/map.tsx
+++ ui/src/services/map.tsx
@@ -1,20 +1,31 @@
+import { ROUTE_TYPES } from "../constants/identifiers";
 
 const generateRouteWithStartAndFinish = async(
 origin: google.maps.LatLng, 
 destination: google.maps.LatLng, 
 DirectionsService: google.maps.DirectionsService,
 DirectionsRenderer: google.maps.DirectionsRenderer,
-routeMap: google.maps.Map) => {
+routeMap: google.maps.Map,
+type: ROUTE_TYPES) => {
     if (DirectionsService != undefined && DirectionsRenderer != undefined && routeMap != undefined) {
-      var response = await DirectionsService.route({
-          origin: origin,
-          destination: destination,
-          optimizeWaypoints: true,
-          unitSystem: google.maps.UnitSystem.IMPERIAL,
-          travelMode: google.maps.TravelMode.BICYCLING,
-      });
+        let travelMode : google.maps.TravelMode;
+        if (type == 1) {
+            travelMode = google.maps.TravelMode.WALKING;
+        } else if (type == 2) {
+            travelMode = google.maps.TravelMode.BICYCLING;
+        } else {
+            travelMode = google.maps.TravelMode.WALKING;
+        }
 
-      return  response;
+        var response = await DirectionsService.route({
+            origin: origin,
+            destination: destination,
+            optimizeWaypoints: true,
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+            travelMode: travelMode,
+        });
+
+        return  response;
     }
 }
 
